ec9a2b610d1f9fb466c2c25b39416e8cd701ffcf
Amir Ayub <amirayubx@gmail.com>
2020-09-25 23:22:32 +0300

    Migrate to es6 and Webstorm

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+import mongoose from 'mongoose';
+import User from './models/user.js';
+
+const connectionString = 'mongodb+srv://amir:2904@cluster0.t1yta.mongodb.net/tasks-app?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const amir = new User({
+    name: 'Amir',
+    age: 'a'
+});
+
+amir.save().then((model) => {
+    console.log(model);
+}).catch((err) => {
+    console.log((err));
+});
---
+++ models/user.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number');
+            }
+        }
+    }
+});
+
+export default User;
